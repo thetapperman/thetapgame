6b12addfff7a55d12a6052ab562c0b31eab2a134
Steinar Kraugerud <=>
2016-08-03 22:09:49 +0200

    Succesfully implemented a backend solution for the horseRace-game

--- src/thetapgame/HorseRace.java
+++ src/thetapgame/HorseRace.java
@@ -7,7 +7,9 @@ package thetapgame;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.Map;
 import javax.swing.JLabel;
 
 /**
@@ -16,39 +18,65 @@ import javax.swing.JLabel;
  */
 public class HorseRace {
     private int rows;
-    private HashMap <Card, Integer> aces; //card: position
-    private HashMap <Card, Boolean> deck; //card: isUsed
-    private HashMap <Card, String> playCards; //card: String punishment
-    
-    public HorseRace(int rows){
-        this.rows = rows;
-        this.aces = initAces();
+    private ArrayList<Card> deck; //card: isUsed
+    private HashMap <Card, Integer> aces;
+   
+    public HorseRace(){
+        this.rows = 5;
         this.deck = initDeck();
-        this.playCards = initPlayCards();
-    }
-    
-    public HashMap <Card, Boolean> initPlayCards(){
-        ArrayList<Card> playcards = new ArrayList<Card>();
-        
-        for(int cardNum=0;cardNum<this.rows;cardNum++){
-        
-        }
+        shuffleDeck();
+        this.aces = initAces();
     }
     
     public HashMap <Card, Integer> initAces(){
-        HashMap <Card, Integer> aces = new HashMap<Card,Integer>();
+        HashMap <Card, Integer> aces = new HashMap <Card, Integer>();
         String[] suits = {"spades", "hearts", "diamonds", "clubs"};
         String rank = "A";
-        for(int cardNumber = 0;cardNumber<suits.length;cardNumber++){
-            Card ace = new Card(rank,suits[cardNumber]);
-            aces.put(ace,0);
+        
+        for(int i= 0;i<suits.length;i++){
+            Card card = new Card(rank,suits[i]);
+            aces.put(card, 0);
         }
+        
         return aces;
     }
-   
     
-    public HashMap <Card, Boolean> initDeck(){
-        HashMap <Card, Boolean> deck  = new HashMap<Card,Boolean>();
+    public void updatePositionOfAce(String rank,String suit){
+        for (Map.Entry<Card, Integer> iterator : this.aces.entrySet()) {
+            this.aces.put(iterator.getKey(),iterator.getValue()+1);
+        }
+    }
+    
+    public void shuffleDeck(){
+        Collections.shuffle(this.deck);
+    }
+    
+    public void moveCardFromDeckTodrawnCards(String rank, String suit){
+        if(this.deck.size()>0){
+            for(int i = 0;i<this.deck.size();i++){
+                if(this.deck.get(i).getRank().equals(rank) && this.deck.get(i).getSuit().equals(suit)){
+                    this.deck.remove(this.deck.get(i));
+                }
+            }
+        }
+    }
+   
+   public Card getRandomCard(){
+       
+        if(this.deck.size()==0){
+            this.deck = initDeck();
+            shuffleDeck();
+        }
+            Card card = this.deck.get(0);
+            String rank = card.getRank();
+            String suit = card.getSuit();
+            moveCardFromDeckTodrawnCards(rank,suit);
+            
+            return card;
+    }
+    
+    public ArrayList<Card> initDeck(){
+        ArrayList<Card> deck = new ArrayList<Card>();
         
         String[] suits = {"spades", "hearts", "diamonds", "clubs"};
         String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
@@ -56,20 +84,154 @@ public class HorseRace {
         for(int rank = 0;rank<ranks.length;rank++){
             for(int suit = 0;suit<suits.length;suit++){
                 Card card = new Card(ranks[rank],suits[suit]);
-                deck.put(card,false);
+                deck.add(card);
             }
         }
         return deck;
     }
     
+    public String checkIfWinner(){
+        String winnerSuit = "";
+        for (Map.Entry<Card, Integer> iterator : this.aces.entrySet()) {
+            if(iterator.getValue()==this.rows+1){
+                winnerSuit = iterator.getKey().getSuit();
+            }
+        }
+        return winnerSuit;
+    }
     
+    public void updatePositions(String suit){
+        for (Map.Entry<Card, Integer> iterator : this.aces.entrySet()) {
+            if(iterator.getKey().getSuit().equals(suit)){
+                this.aces.put(iterator.getKey(),iterator.getValue()+1);
+            }
+        } 
+    }
     
-    public void startHorseRace(){
+    public boolean isTurnTime(int highestTurnedCard){
+        boolean isPunishment = true;
+        
+        for (Map.Entry<Card, Integer> iterator : this.aces.entrySet()) {
+            if(iterator.getValue()<highestTurnedCard){
+                isPunishment = false;
+            }
+        }
+        return isPunishment;
+    }
     
+    public void punishGuilty(String suit){
+        for (Map.Entry<Card, Integer> iterator : this.aces.entrySet()) {
+            if(iterator.getKey().getSuit().equals(suit)){
+                this.aces.put(iterator.getKey(),iterator.getValue()-1);
+                System.out.println("\n\nloosing position_______________: "+iterator.getKey().getSuit());
+            }
+        } 
+    }
+    
+    public int updateHighestReachedCard(){
+        return Collections.max(this.aces.values());
+    }
+    
+    public void printPositions(){
+        for (Map.Entry<Card, Integer> iterator : this.aces.entrySet()) {
+            System.out.println("Suit: "+iterator.getKey().getSuit());
+            System.out.println("Position: "+iterator.getValue());
+           
+        } 
+         System.out.println("\n\n_______________________________________________________");
+    }
+    
+    public int updateHighestTurned(){
+        return Collections.min(this.aces.values());
+    }
+    
+    public void punishDrugathic(String suit){
+        for (Map.Entry<Card, Integer> iterator : this.aces.entrySet()) {
+            if(iterator.getKey().getSuit().equals(suit)){
+                System.out.println("----------------->guilty-------------->"+suit);
+                this.aces.put(iterator.getKey(),0);
+            }
+        } 
+    }
+    
+    public String returnGuiltySuit(int highestReached){
+        String suit = "";
+        for (Map.Entry<Card, Integer> iterator : this.aces.entrySet()) {
+            if(iterator.getValue()==highestReached){
+                suit=iterator.getKey().getSuit();
+            }
+        } 
+        return suit;
+    }
+    
+    public HashMap <Integer,Boolean> initPunishMap(){
+        HashMap <Integer,Boolean> p = new HashMap <Integer,Boolean>();
+       
+        for(int i = 2;i<this.rows+1;i++){
+            if(i% 2 == 1){
+                p.put(i,false);
+            }
+        }
+        
+        for (Map.Entry<Integer, Boolean> iterator : p.entrySet()) {
+            System.out.println("card: "+iterator.getKey());
+            System.out.println("isPunished: "+iterator.getValue());
+        } 
+        
+        return p;
+    }
+    
+    public String startHorseRace(){
+        boolean isFinished = false;
+        
+        ArrayList<Integer> turnedCards = new ArrayList<Integer>();
+        int highestReachedCard = 0;
+        
+        HashMap <Integer,Boolean> punishMap = new HashMap <Integer,Boolean>();
+        punishMap = initPunishMap();
+        
+        String suit = "";
+        while(!isFinished){
+            
+            int highest = updateHighestTurned();
+            if(!turnedCards.contains(highest)&&highest!=0){//Hvis ikke kortet er flippet og alle har passert->flip. Straff de 
+                turnedCards.add(highest);
+                Card punishmentCard = getRandomCard();
+                System.out.println("turned card: ____________"+punishmentCard.getSuit()+"______________");
+                punishGuilty(punishmentCard.getSuit());
+            }else{ //Gå videre
+                Card card = getRandomCard();
+                updatePositions(card.getSuit());
+                highestReachedCard = updateHighestReachedCard();
+                System.out.println("highest reached card: "+highestReachedCard);
+            }
+            
+            //Dopingtest:
+            if(punishMap.containsKey(highestReachedCard) && punishMap.get(highestReachedCard)==false){
+                punishMap.put(highestReachedCard,true);
+                Card drugCard = getRandomCard();
+                System.out.println("Checking for drugs...");
+                    if(returnGuiltySuit(highestReachedCard).equals(drugCard.getSuit())){
+                        punishDrugathic(drugCard.getSuit());
+                        System.out.println("................punishing:..................");
+                    }
+            }
+            
+            
+            String check = checkIfWinner();
+            if(check!=""){
+                isFinished = true;
+                suit = check;
+            }
+            printPositions();
+        }
+        return suit;
     }
     
      public static void main(String[] args) throws IOException {
-        HorseRace horse = new HorseRace(10);
+        HorseRace horse = new HorseRace();
+        System.out.println("Starting tour");
+        String winner = horse.startHorseRace();
+        System.out.println("Winner: "+winner);
     }
-    
 }
