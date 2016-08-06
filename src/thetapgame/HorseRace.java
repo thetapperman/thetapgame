c4c8d17c60f0733d7994426f9f817dd95c7c9b97
Steinar Kraugerud <=>
2016-08-07 01:27:57 +0200

    Horserace works

--- src/thetapgame/HorseRace.java
+++ src/thetapgame/HorseRace.java
@@ -5,11 +5,15 @@
  */
 package thetapgame;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 
 /**
@@ -20,12 +24,138 @@ public class HorseRace {
     private int rows;
     private ArrayList<Card> deck; //card: isUsed
     private HashMap <Card, Integer> aces;
+    private ImageIcon img;
+    private JFrame f;
+    private HashMap <Integer, JLabel> labelHashingNormalBackCards;
+    private HashMap <Integer, JLabel> labelHashingDrugBackCards;
+    private HashMap <String, JLabel> labelHashingAces;
+    private HashMap <String, Integer> acePos;
    
-    public HorseRace(){
+    public HorseRace() throws IOException{
         this.rows = 5;
         this.deck = initDeck();
         shuffleDeck();
         this.aces = initAces();
+        this.f = new JFrame();
+        this.img = new ImageIcon(ImageIO.read(new File("horse_background.jpg")));
+        createBoard();
+        this.labelHashingNormalBackCards = createLabelHashingNormalBack();
+        this.labelHashingDrugBackCards = createLabelHashingDrugs();
+        this.labelHashingAces = createLabelHashingAces();
+        startHorseRace();
+        
+    }
+   
+    public void printBackCardsLabel(){
+        for (Map.Entry<Integer, JLabel> iterator : this.labelHashingNormalBackCards.entrySet()) {
+            System.out.println(iterator.getKey()+":"+iterator.getValue());
+        } 
+    }
+    
+    public void moveAce(int index, String suit){
+        this.labelHashingAces.get(suit).setBounds(this.acePos.get(suit), 360 - index*100, this.img.getIconWidth(), this.img.getIconHeight());
+    }
+    
+     public HashMap <String, JLabel> createLabelHashingAces(){
+        HashMap <String, JLabel> labelHashingAces = new HashMap <String, JLabel>();
+        HashMap <String, Integer> acePos = new HashMap <String, Integer>();
+         String[] suits = {"spades", "hearts", "diamonds", "clubs"};
+        
+         for(int i = 0;i<suits.length;i++){
+             String imagePath = "Ah";
+             imagePath+=suits[i];
+             imagePath+=".gif";
+             labelHashingAces.put(suits[i], paintRouteCards(imagePath,-100+i*100,360));
+             acePos.put(suits[i], -100+i*100);
+             
+         }
+        this.acePos = acePos;
+        return labelHashingAces;
+    }
+    
+    public HashMap <Integer, JLabel> createLabelHashingDrugs(){
+        HashMap <Integer, JLabel> hash = new HashMap <Integer, JLabel>();
+        
+        for(int i = 1;i<this.rows;i++){
+            if(i% 2 == 0){
+                hash.put(i,paintRouteCards("backhcard.gif",-280,260-(i)*100));
+            }
+        }
+        return hash;
+    }
+    
+    public HashMap <Integer, JLabel> createLabelHashingNormalBack(){
+        HashMap <Integer, JLabel> hash = new HashMap <Integer, JLabel>();
+        
+        for(int i = 0;i<this.rows;i++){
+            
+            hash.put(i+1,paintRouteCards("backhcard.gif",-200,260-i*100));
+        }
+        
+        
+        return hash;
+        
+    }
+    
+    public void setNormalBackCardInvisible(int nr){
+        this.labelHashingNormalBackCards.get(nr).setVisible(false);
+    }
+    
+    public void setDrugCardInvisible(int nr){
+        this.labelHashingDrugBackCards.get(nr).setVisible(false);
+        
+    }
+    
+    public JLabel paintRouteCards(String card,int x,int y){
+        
+            JLabel lbl = new JLabel(new ImageIcon(card));
+            lbl.setBounds(x, y, this.img.getIconWidth(), this.img.getIconHeight());
+            
+            this.f.add(lbl);
+            lbl.setVisible(true);
+            this.f.repaint();
+            
+            return lbl;
+    }
+    
+    public void displayDrawnBackCard(Card card, int nr){
+        setNormalBackCardInvisible(nr);
+        
+        JLabel lbl = new JLabel(new ImageIcon(card.getRank()+"h"+card.getSuit()+".gif"));
+        
+        /*if(nr==1){//special case 
+            lbl.setBounds(-200, 260, this.img.getIconWidth(), this.img.getIconHeight());
+        }else{
+            lbl.setBounds(-200, 260-(nr-1)*100, this.img.getIconWidth(), this.img.getIconHeight());
+        }*/
+        
+        lbl.setBounds(-200, 260-(nr-1)*100, this.img.getIconWidth(), this.img.getIconHeight());
+        this.f.add(lbl);
+        lbl.setVisible(true);
+        this.f.repaint();
+        sleepProgram(1);
+    }
+    
+    public void displayHorseDone(String winner){
+        AppearWindow stateSlurps = new AppearWindow("HorseRace complete"); //rams opp straffeslurker her
+        stateSlurps.ShowFrame(true);
+        stateSlurps.whileConnected();
+        
+    }
+    
+    public void gg(){
+        for (Map.Entry<Integer, JLabel> iterator : this.labelHashingDrugBackCards.entrySet()) {
+            System.out.println("drug: "+iterator.getKey()+", "+iterator.getValue());
+        }
+    }
+    
+    public void createBoard() {
+        this.f.setContentPane(new JLabel(this.img));
+    	this.f.pack();
+        this.f.setLayout(null);
+        this.f.setResizable(false);
+        this.f.setDefaultCloseOperation(0);
+        this.f.setVisible(true);
     }
     
     public HashMap <Card, Integer> initAces(){
@@ -60,8 +190,23 @@ public class HorseRace {
             }
         }
     }
+    
+   public void paintDrawCard(String path){
+        
+        JLabel lbl = new JLabel(new ImageIcon(path));
+        lbl.setBounds(100, -200, this.img.getIconWidth(), this.img.getIconHeight());
+
+        this.f.add(lbl);
+        lbl.setVisible(true);
+        this.f.repaint();
+        sleepProgram(1);
+        this.f.add(lbl);
+        lbl.setVisible(false);
+        this.f.repaint();
+        sleepProgram(1);
+   }
    
-   public Card getRandomCard(){
+   public Card getRandomCard(boolean flag){
        
         if(this.deck.size()==0){
             this.deck = initDeck();
@@ -71,6 +216,11 @@ public class HorseRace {
             String rank = card.getRank();
             String suit = card.getSuit();
             moveCardFromDeckTodrawnCards(rank,suit);
+            
+            if(flag){
+                String cardPath = card.getRank()+card.getSuit()+".jpg";
+                paintDrawCard(cardPath);
+            }
             
             return card;
     }
@@ -154,6 +304,7 @@ public class HorseRace {
         } 
     }
     
+    
     public String returnGuiltySuit(int highestReached){
         String suit = "";
         for (Map.Entry<Card, Integer> iterator : this.aces.entrySet()) {
@@ -181,38 +332,99 @@ public class HorseRace {
         return p;
     }
     
-    public String startHorseRace(){
+    public void sleepProgram(int seconds){
+        int milliSeconds = seconds*1000;
+        
+        try {
+            Thread.sleep(milliSeconds);                 
+        } catch(InterruptedException ex) {
+            Thread.currentThread().interrupt();
+        }
+    }
+    
+    public void printAces(){
+         for (Map.Entry<String, Integer> iterator : this.acePos.entrySet()) {
+            System.out.println("ace: "+iterator.getValue());
+        } 
+    }
+    
+    public int getIndexByKey(String suit){
+        int index=0;
+        for (Map.Entry<Card, Integer> iterator : this.aces.entrySet()) {
+            if(iterator.getKey().getSuit()==suit){
+                index = iterator.getValue();
+            }
+        } 
+        return index;
+    }
+    
+    public void setVisibleDrugCard(int nr,Card drugCard){
+        String path=drugCard.getRank()+"h"+drugCard.getSuit()+".gif";
+        JLabel lbl = new JLabel(new ImageIcon(path));
+        
+        lbl.setBounds(-280, 260-(nr)*100, this.img.getIconWidth(), this.img.getIconHeight());
+        this.f.add(lbl);
+        lbl.setVisible(true);
+        this.f.repaint();
+     
+        sleepProgram(1);
+    }
+    
+    public void startHorseRace(){
+        sleepProgram(1);
+        String winner ="";
         boolean isFinished = false;
         
         ArrayList<Integer> turnedCards = new ArrayList<Integer>();
         int highestReachedCard = 0;
         
+        ArrayList<Integer> punMap = new ArrayList<Integer>();
         HashMap <Integer,Boolean> punishMap = new HashMap <Integer,Boolean>();
         punishMap = initPunishMap();
         
         String suit = "";
         while(!isFinished){
-            
+           // printBackCardsLabel();
             int highest = updateHighestTurned();
             if(!turnedCards.contains(highest)&&highest!=0){//Hvis ikke kortet er flippet og alle har passert->flip. Straff de 
                 turnedCards.add(highest);
-                Card punishmentCard = getRandomCard();
+                Card punishmentCard = getRandomCard(false);
+                displayDrawnBackCard(punishmentCard,highest);
                 System.out.println("turned card: ____________"+punishmentCard.getSuit()+"______________");
+                sleepProgram(1);
                 punishGuilty(punishmentCard.getSuit());
-            }else{ //Gå videre
-                Card card = getRandomCard();
-                updatePositions(card.getSuit());
-                highestReachedCard = updateHighestReachedCard();
-                System.out.println("highest reached card: "+highestReachedCard);
+                int index = getIndexByKey(punishmentCard.getSuit());
+                
+                moveAce(index, punishmentCard.getSuit());
+                sleepProgram(1);
             }
+
+            Card card = getRandomCard(true);
+            updatePositions(card.getSuit());
+
+            int index = getIndexByKey(card.getSuit());
+            moveAce(index, card.getSuit());
+            sleepProgram(1);
+            highestReachedCard = updateHighestReachedCard();
+            System.out.println("highest reached card: "+highestReachedCard);
+            
             
             //Dopingtest:
-            if(punishMap.containsKey(highestReachedCard) && punishMap.get(highestReachedCard)==false){
-                punishMap.put(highestReachedCard,true);
-                Card drugCard = getRandomCard();
+            if(!punMap.contains(highestReachedCard) && highestReachedCard%2==1 && highestReachedCard>1 ){
+                //punishMap.put(highestReachedCard,true);
+                punMap.add(highestReachedCard);
+                
+                Card drugCard = getRandomCard(false);
                 System.out.println("Checking for drugs...");
+                System.out.println("highestReached: "+highestReachedCard);
+                gg();
+                setDrugCardInvisible(highestReachedCard-1);
+                setVisibleDrugCard(highestReachedCard-1,drugCard);
                     if(returnGuiltySuit(highestReachedCard).equals(drugCard.getSuit())){
+                        
                         punishDrugathic(drugCard.getSuit());
+                        moveAce(0, drugCard.getSuit());
+                        sleepProgram(1);
                         System.out.println("................punishing:..................");
                     }
             }
@@ -225,13 +437,15 @@ public class HorseRace {
             }
             printPositions();
         }
-        return suit;
+        winner = suit;
+        
+        displayHorseDone(winner);
+        this.f.setVisible(false);
+        
     }
+    
     
      public static void main(String[] args) throws IOException {
         HorseRace horse = new HorseRace();
-        System.out.println("Starting tour");
-        String winner = horse.startHorseRace();
-        System.out.println("Winner: "+winner);
     }
 }
